30201f0ab979da57cad273d2851add584c5ee46d
avazjanovsardorbek01 <sardoravazzanov@gmail.com>
2024-07-24 14:58:21 +1000

    Sport-Market

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,64 @@
-import Hero from "@/components/home/Hero";
-import Trending from "@/components/home/TrendingCollection";
-import TopCreator from "@/components/home/TopCreators"
-import Category from "@/components/home/BrowseCategory"
-import Discover from "@/components/home/Discover"
-import Works from "@/components/home/HowItWorks";
-import Mashroom from "@/components/home/MagicMashroom";
-import Message from "@/components/home/Message"
 
+
+import { Carusel , CaruselCatalog ,  CaruselProduct} from "@/components/ui";
+import { Card1 } from "@/components/ui/card";
+import {SectionHome} from "@/components/sections"
 
 export default function Home() {
   return (
-    <div className="px-12 xl:px-32 md:px-20">
-      <div className="flex flex-col gap-36">
-        <Hero />
-        <Trending />
-        <TopCreator />
-        <Category />
-        <Discover />
-        <div className="mx-auto w-full flex justify-center mt-20">
-          <Mashroom />
+    <main className="">
+      <section  className=" pt-5 pb-[83px] bg-[#F2F2F2]">
+        <div className=" container mx-auto px-2.5">
+            <div className="flex items-start gap-[24px]">
+              <div className=" min-w-[819px] h-[488px]">
+                <Carusel />
+              </div>
+              <div className="">
+                  <Card1/> 
+              </div>
+
+            </div>
         </div>
-        <Works />
-        <Message />
-      </div>
-    </div>
+      </section>
+      <section>
+        <div className="container mx-auto px-2 5">
+          <div className="py-[24px]">
+            <h1 className="text-[32px] mb-[31px]">Катаолог</h1>
+            <div>
+              <CaruselCatalog />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="bg-[#F2F2F2] pb-[80px] ">
+        <div className="container mx-auto px-2 5">
+          <div className="pt-[76px]">
+            <h1 className="text-[32px] mb-[31px]">Акция</h1>
+            <div>
+              <CaruselProduct text="Акция" bg="#FF1C1C"/>
+            </div>
+          </div>
+          <div className="pt-[76px]">
+            <h1 className="text-[32px] mb-[31px]">Новинки</h1>
+            <div>
+              <CaruselProduct text="Новый" bg="#917BFF"/>
+            </div>
+          </div>
+          <div className="pt-[76px]">
+            <h1 className="text-[32px] mb-[31px]">Продукты</h1>
+            <div>
+              <CaruselProduct />
+            </div>
+          </div>
+          <div className="pt-[76px]">
+            <h1 className="text-[32px] mb-[31px]">ТОП продажа</h1>
+            <div>
+              <CaruselProduct text="ТОП" bg="#10DE24" />
+            </div>
+          </div>
+        </div>
+      </section>
+      <SectionHome/>
+    </main>
   );
 }
